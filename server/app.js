63a8716750ccea21eb9dcf86ff1335439a780953
Justin O'Connor <justin.oc03@gmail.com>
2016-09-22 13:43:18 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+//express is actually a function and needs to be able to be called later on...
+var app = express();
+
+//similar to the usage of anything.data()
+//process.env is a port setting utility that some systems use, like heroku
+// LOOK AT MY NOTES FOR INFO ABOUT .data
+app.set('port', (process.env.port || 5000) );
+
+app.get('/*', function(req, res){
+  var file = req.params[0] || '/views/index/html';
+  res.sendFile(path.join(__dirname,"/public/", file));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Turned on the thing!: ', app.get('port'));
+});
